c2f1966ad10117a0db606c20c5fa8a14f133bb64
Mjau <anulajal@gmail.com>
2015-08-20 16:05:08 +0300

    Status added

--- Koodimine/src/main/java/ee/anu/server/Status.java
+++ Koodimine/src/main/java/ee/anu/server/Status.java
@@ -4,7 +4,8 @@ public enum Status {
 
     OK(200, "OK"),
     BadRequest(400, "Bad Request"),
-    NotFound(404, "Not Found");
+    NotFound(404, "Not Found"),
+    InternalError(500, "Internal Server Error");
 
     private int code;
     private String message;
